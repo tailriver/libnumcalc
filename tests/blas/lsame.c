e9937876cbb90af252a3570e30474d81f967276e
Shinsuke Ogawa <shinsuke@tailriver.net>
2013-08-11 17:35:08 +0900

    add test for lsame

---
+++ tests/blas/lsame.c
@@ -0,0 +1,20 @@
+#include <numcalc/blas.h>
+#include <stdio.h>
+
+int main(void) {
+    unsigned char c;
+    size_t expected, got, i;
+
+    expected = 0;
+    got = 0;
+    c = 0;
+    while (1) {
+        expected += c == 'N' || c == 'n';
+        got += lsame('N', c);
+        if (expected != got || c == 255) break;
+        c++;
+    }
+    printf("expected: %ld, got: %ld\n", expected, got);
+
+    return 0;
+}
